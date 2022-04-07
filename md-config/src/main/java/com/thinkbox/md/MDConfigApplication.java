5dedfd384ec63fb1f63e59209709e55b42286f22
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2022-04-07 16:52:15 -0400

    add config server

---
+++ md-config/src/main/java/com/thinkbox/md/MDConfigApplication.java
@@ -0,0 +1,15 @@
+package com.thinkbox.md;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MDConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MDConfigApplication.class, args);
+	}
+
+}
