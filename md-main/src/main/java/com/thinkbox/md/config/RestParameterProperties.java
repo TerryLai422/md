afbfd0cee579fbfaa3dc868e28e8f0ceefc61840
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2022-04-18 21:10:58 -0400

    remove constructor annotation

--- md-main/src/main/java/com/thinkbox/md/config/RestParameterProperties.java
+++ md-main/src/main/java/com/thinkbox/md/config/RestParameterProperties.java
@@ -6,15 +6,11 @@ import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Configuration
 @ConfigurationProperties(prefix = "rest-parameter")
 public class RestParameterProperties {
