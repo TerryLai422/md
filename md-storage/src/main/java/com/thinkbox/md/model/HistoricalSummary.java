000286b3f2aa4cadbb24d3588e940bf47c7ca93a
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2022-04-25 23:46:59 -0400

    add implementation for aggregation

---
+++ md-storage/src/main/java/com/thinkbox/md/model/HistoricalSummary.java
@@ -0,0 +1,24 @@
+package com.thinkbox.md.model;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HistoricalSummary {
+
+	@Id
+	private String ticker;
+	
+	private Long total;
+	
+	private String firstDate;
+	
+	private String lastDate;
+}
--- md-storage/src/main/java/com/thinkbox/md/repository/HistoricalRepository.java
+++ md-storage/src/main/java/com/thinkbox/md/repository/HistoricalRepository.java
@@ -3,25 +3,31 @@ package com.thinkbox.md.repository;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import com.thinkbox.md.model.Historical;
+import com.thinkbox.md.model.HistoricalSummary;
 
 public interface HistoricalRepository extends MongoRepository<Historical, String> {
 
 	@Query("{ 'ticker': {$exists: false} }")
 	List<Historical> findAllNull();
-	
+
 	@Query("{ 'ticker': {$exists: false} }")
 	List<Historical> findAllNull(Pageable pageable);
 
 	List<Historical> findByTicker(String ticker);
 
 	List<Historical> findBySymbol(String symbol);
-	
+
 	long countByTicker(String ticker);
 
 	long countBySymbol(String symbol);
+
+	@Aggregation(pipeline = { "{$match: {ticker:?0}}",
+			"{$group: { _id : $ticker, total: {$sum:1}, firstDate : { $max : $date}}}" })
+	List<HistoricalSummary> getSummary(String ticker);
 
 }
