95e63ba1ffeabfe233f84c9470a75bf27bd741a5
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2022-04-28 22:32:46 -0400

    add field historical high/low date for instrument class

--- md-storage/src/main/java/com/thinkbox/md/mapper/DataMapper.java
+++ md-storage/src/main/java/com/thinkbox/md/mapper/DataMapper.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.thinkbox.md.config.MapKeyParameter;
+import com.thinkbox.md.model.Analysis;
 import com.thinkbox.md.model.Historical;
 import com.thinkbox.md.model.HistoricalSummary;
 import com.thinkbox.md.model.Instrument;
@@ -26,8 +27,12 @@ public class DataMapper {
 
 	public Historical convertMapToHistorical(Map<String, Object> map) {
 
-		Historical historical = new Historical();
+		return convertMapToHistorical(map, new Historical());
 
+	}
+
+	private Historical convertMapToHistorical(Map<String, Object> map, Historical historical) {
+		
 		historical.setId(map.get(mapKey.getType()) + "-" + map.get(mapKey.getSymbol()) + "@" + map.get(mapKey.getDate())
 				+ "-" + map.get(mapKey.getTime()));
 		historical.setInterval((String) map.get(mapKey.getInterval()));
@@ -48,15 +53,15 @@ public class DataMapper {
 		historical.setAdjClose((Double) map.get(mapKey.getAdjClose()));
 		historical.setVolume(Long.valueOf(map.getOrDefault(mapKey.getVolume(), DEFAULT_LONG_VALUE).toString()));
 		historical.setOthers(map);
-
+		
 		return historical;
 	}
 
 	public Instrument convertMapToInstrument(Map<String, Object> map) {
 
 		Instrument instrument = null;
-		
-		String subExchange  = map.getOrDefault(mapKey.getSubExchange(), "-").toString();
+
+		String subExchange = map.getOrDefault(mapKey.getSubExchange(), "-").toString();
 		if (subExchange.equals("TSX") || subExchange.equals("TSXV")) {
 			instrument = new InstrumentCA();
 		} else {
@@ -76,26 +81,35 @@ public class DataMapper {
 		instrument.setSector((String) map.getOrDefault(mapKey.getSector(), DEFAULT_STRING_VALUE));
 		instrument.setType((String) map.getOrDefault(mapKey.getType(), DEFAULT_STRING_VALUE));
 		instrument.setBeta(Double.valueOf(map.getOrDefault(mapKey.getBeta(), DEFAULT_DOUBLE_VALUE).toString()));
-		instrument.setForwardPE(Double.valueOf(map.getOrDefault(mapKey.getForwardPE(), DEFAULT_DOUBLE_VALUE).toString()));
+		instrument
+				.setForwardPE(Double.valueOf(map.getOrDefault(mapKey.getForwardPE(), DEFAULT_DOUBLE_VALUE).toString()));
 		instrument.setSharesOutstanding(
 				Long.valueOf(map.getOrDefault(mapKey.getSharesOutstanding(), DEFAULT_LONG_VALUE).toString()));
 		instrument.setMarketCap(
 				Long.valueOf(map.getOrDefault(mapKey.getSharesOutstanding(), DEFAULT_LONG_VALUE).toString()));
 		instrument.setHistoricalTotal(
 				Long.valueOf(map.getOrDefault(mapKey.getHistoricalTotal(), DEFAULT_LONG_VALUE).toString()));
-		instrument.setHistoricalFirstDate((String) map.getOrDefault(mapKey.getHistoricalFirstDate(), DEFAULT_STRING_VALUE));
-		instrument.setHistoricalLastDate((String) map.getOrDefault(mapKey.getHistoricalLastDate(), DEFAULT_STRING_VALUE));
-		instrument.setHistoricalHigh(Double.valueOf(map.getOrDefault(mapKey.getHistoricalHigh(), DEFAULT_DOUBLE_VALUE).toString()));
-		instrument.setHistoricalLow(Double.valueOf(map.getOrDefault(mapKey.getHistoricalLow(), DEFAULT_DOUBLE_VALUE).toString()));
+		instrument.setHistoricalFirstDate(
+				(String) map.getOrDefault(mapKey.getHistoricalFirstDate(), DEFAULT_STRING_VALUE));
+		instrument
+				.setHistoricalLastDate((String) map.getOrDefault(mapKey.getHistoricalLastDate(), DEFAULT_STRING_VALUE));
+		instrument.setHistoricalHigh(
+				Double.valueOf(map.getOrDefault(mapKey.getHistoricalHigh(), DEFAULT_DOUBLE_VALUE).toString()));
+		instrument.setHistoricalLow(
+				Double.valueOf(map.getOrDefault(mapKey.getHistoricalLow(), DEFAULT_DOUBLE_VALUE).toString()));
+		instrument.setHistoricalHighDate((String) map.getOrDefault(mapKey.getHistoricalHighDate(), DEFAULT_STRING_VALUE));
+		instrument.setHistoricalLowDate((String) map.getOrDefault(mapKey.getHistoricalLowDate(), DEFAULT_STRING_VALUE));
+
 		instrument.setOthers(map);
-		
+
 		return instrument;
 	}
 
 	public Map<String, Object> convertHistoricalSummaryToMap(HistoricalSummary summary) {
 
-		Map<String, Object> map = new TreeMap<>();;
-		
+		Map<String, Object> map = new TreeMap<>();
+		;
+
 		if (summary != null) {
 			map.put(mapKey.getTicker(), summary.getTicker());
 			map.put(mapKey.getHistoricalTotal(), summary.getTotal());
