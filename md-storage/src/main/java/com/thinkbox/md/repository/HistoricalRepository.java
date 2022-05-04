eab44b3853fd476262270c89f68523234a3abe86
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2022-05-04 16:35:31 -0400

    rearrange spacing for query

--- md-storage/src/main/java/com/thinkbox/md/repository/HistoricalRepository.java
+++ md-storage/src/main/java/com/thinkbox/md/repository/HistoricalRepository.java
@@ -12,22 +12,22 @@ import com.thinkbox.md.model.HistoricalSummary;
 
 public interface HistoricalRepository extends MongoRepository<Historical, String> {
 
-	@Query("{ ticker: {$exists: false} }")
+	@Query("{ticker: {$exists:false}}")
 	List<Historical> findAllNull();
 
-	@Query("{ ticker: {$exists: false} }")
+	@Query("{ticker: {$exists:false}}")
 	List<Historical> findAllNull(Pageable pageable);
 
 	List<Historical> findByTicker(String ticker);
 
-	@Query(value= "{ ticker: {$eq: ?0},  date: {$gte: ?1}}", sort="{date:1}")
+	@Query(value= "{ticker:{$eq:?0}, date:{$gte:?1}}", sort="{date:1}")
 	List<Historical> findByTickerAndDate(String ticker, String date);
 
 	long countByTicker(String ticker);
 
 	@Aggregation(pipeline = { "{$match: {ticker:?0}}",
-	"{$project: {_id:$ticker, closeDate: {$concat:[{$toString: $close}, '-', $date]}, date:$date, close:$close }}", 
-	"{$group: {_id :$_id, total:{$sum: 1}, firstDate:{$min: $date}, lastDate:{$max: $date}, high:{$max: $closeDate}, low:{$min: $closeDate}, lastP:{$last: $close} }}" })	
+	"{$project:{_id:$ticker, closeDate: {$concat:[{$toString: $close}, '-', $date]}, date:$date, close:$close}}", 
+	"{$group:{_id :$_id, total:{$sum:1}, firstDate:{$min:$date}, lastDate:{$max:$date}, high:{$max:$closeDate}, low:{$min:$closeDate}, lastP:{$last:$close}}}" })	
 	List<HistoricalSummary> getSummary(String ticker);
 
 }
